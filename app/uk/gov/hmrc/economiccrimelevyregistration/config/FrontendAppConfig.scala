8679388b5832ab7f7f263b5883be4910c1803323
peter-hazell <18005888+peter-hazell@users.noreply.github.com>
2022-08-04 13:10:04 +0100

    Added initial scaffold code

---
+++ app/uk/gov/hmrc/economiccrimelevyregistration/config/FrontendAppConfig.scala
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.economiccrimelevyregistration.config
+
+import com.google.inject.{Inject, Singleton}
+import play.api.Configuration
+import play.api.i18n.Lang
+import play.api.mvc.RequestHeader
+import uk.gov.hmrc.play.bootstrap.binders.SafeRedirectUrl
+
+@Singleton
+class FrontendAppConfig @Inject() (configuration: Configuration) {
+
+  val host: String    = configuration.get[String]("host")
+  val appName: String = configuration.get[String]("appName")
+
+  private val contactHost = configuration.get[String]("contact-frontend.host")
+  private val contactFormServiceIdentifier = "economic-crime-levy-registration-frontend"
+
+  def feedbackUrl(implicit request: RequestHeader): String =
+    s"$contactHost/contact/beta-feedback?service=$contactFormServiceIdentifier&backUrl=${SafeRedirectUrl(host + request.uri).encodedUrl}"
+
+  val loginUrl: String         = configuration.get[String]("urls.login")
+  val loginContinueUrl: String = configuration.get[String]("urls.loginContinue")
+  val signOutUrl: String       = configuration.get[String]("urls.signOut")
+
+  private val exitSurveyBaseUrl: String = configuration.get[Service]("microservice.services.feedback-frontend").baseUrl
+  val exitSurveyUrl: String             = s"$exitSurveyBaseUrl/feedback/economic-crime-levy-registration-frontend"
+
+  val languageTranslationEnabled: Boolean =
+    configuration.get[Boolean]("features.welsh-translation")
+
+  def languageMap: Map[String, Lang] = Map(
+    "en" -> Lang("en"),
+    "cy" -> Lang("cy")
+  )
+
+  val timeout: Int   = configuration.get[Int]("timeout-dialog.timeout")
+  val countdown: Int = configuration.get[Int]("timeout-dialog.countdown")
+
+  val cacheTtl: Int = configuration.get[Int]("mongodb.timeToLiveInSeconds")
+}
---
+++ app/uk/gov/hmrc/economiccrimelevyregistration/controllers/CheckYourAnswersController.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.economiccrimelevyregistration.controllers
+
+import com.google.inject.Inject
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.economiccrimelevyregistration.controllers.actions.{DataRequiredAction, DataRetrievalAction, IdentifierAction}
+import uk.gov.hmrc.economiccrimelevyregistration.views.html.CheckYourAnswersView
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import uk.gov.hmrc.economiccrimelevyregistration.viewmodels.govuk.summarylist._
+
+class CheckYourAnswersController @Inject()(
+                                            override val messagesApi: MessagesApi,
+                                            identify: IdentifierAction,
+                                            getData: DataRetrievalAction,
+                                            requireData: DataRequiredAction,
+                                            val controllerComponents: MessagesControllerComponents,
+                                            view: CheckYourAnswersView
+                                          ) extends FrontendBaseController with I18nSupport {
+
+  def onPageLoad(): Action[AnyContent] = (identify andThen getData andThen requireData) {
+    implicit request =>
+
+      val list = SummaryListViewModel(
+        rows = Seq.empty
+      )
+
+      Ok(view(list))
+  }
+}
