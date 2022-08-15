1a3ea0ee991d4aa88b65bbcb663c71519b737b53
peter-hazell <18005888+peter-hazell@users.noreply.github.com>
2022-08-15 10:46:13 +0100

    Update comment and pre-initialise runningServer

--- it/uk/gov/hmrc/economiccrimelevyregistration/base/WireMockStubs.scala
+++ it/uk/gov/hmrc/economiccrimelevyregistration/base/WireMockStubs.scala
@@ -11,10 +11,10 @@ import com.github.tomakehurst.wiremock.stubbing.StubMapping
 trait WireMockStubs extends ISpecBase with WireMockHelper with WireMockSupport with WireMockConfig {
 
   /*
-  This is to initialise the app before running any tests, as it is lazy by default in org.scalatestplus.play.BaseOneAppPerSuite.
-  It enables us to include behaviour tests that need to call routes within the `should` part of a test but before `in`.
-  */
-  private val _ = app
+  This is to initialise the app and server before running any tests, as they are lazy by default in org.scalatestplus.play.BaseOneServerPerSuite.
+  It enables us to include behaviour tests that need to run within the `should` part of a test but before `in`.
+   */
+  private val _ = (app, runningServer)
 
   def stubAuthorised(): StubMapping =
     stub(
