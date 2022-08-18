b673907619b164ddfc5dd53e8d4f9850167817dc
peter-hazell <18005888+peter-hazell@users.noreply.github.com>
2022-08-18 12:12:48 +0100

    Re-introduce radio button scaffold

---
+++ .g8/radioButtonPage/app/uk/gov/hmrc/economiccrimelevyregistration/forms/$className$FormProvider.scala
@@ -0,0 +1,18 @@
+package uk.gov.hmrc.economiccrimelevyregistration.forms
+
+import javax.inject.Inject
+
+import uk.gov.hmrc.economiccrimelevyregistration.forms.mappings.Mappings
+import play.api.data.Form
+
+class $className$FormProvider @Inject() extends Mappings {
+
+  def apply(): Form[$className$] =
+    Form(
+      "value" -> enumerable[$className$]("$className;format="
+
+  decap"$.error.required"
+  )
+  )
+
+}
