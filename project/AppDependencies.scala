7d81ca0fdeba5fc226f692942ac5930b6bbe057b
Tony Ogilvie <111900771+TonyCOgilvie@users.noreply.github.com>
2022-09-22 16:59:26 +0100

    Updated dependencies (#5)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,12 +3,12 @@ import sbt._
 object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % "6.4.0",
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "3.22.0-play-28"
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % "7.3.0",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "3.27.0-play-28"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "6.4.0",
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "7.3.0",
     "org.jsoup"               %  "jsoup"                      % "1.14.3",
     "org.scalatestplus"       %% "scalacheck-1-15"            % "3.2.10.0",
     "org.scalatestplus"       %% "mockito-3-4"                % "3.2.10.0",
