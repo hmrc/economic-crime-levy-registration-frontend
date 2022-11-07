262bfe22c21d6243ccb3622189e2e44f892ff7fb
Peter Hazell <18005888+peter-hazell@users.noreply.github.com>
2022-11-07 09:18:36 +0000

    Upgrade bootstrap-play to 7.11.0 (#24)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,12 +3,12 @@ import sbt._
 object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "7.4.0",
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "7.11.0",
     "uk.gov.hmrc" %% "play-frontend-hmrc"         % "3.27.0-play-28"
   )
 
   val test: Seq[ModuleID]    = Seq(
-    "uk.gov.hmrc"         %% "bootstrap-test-play-28"   % "7.4.0",
+    "uk.gov.hmrc"         %% "bootstrap-test-play-28"   % "7.11.0",
     "org.jsoup"            % "jsoup"                    % "1.14.3",
     "org.mockito"         %% "mockito-scala"            % "1.17.12",
     "org.scalatestplus"   %% "scalatestplus-scalacheck" % "3.1.0.0-RC2",
