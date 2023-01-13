51ebbda73f26a334951d77f5fff07e40fadf85d9
Tony Ogilvie <111900771+TonyCOgilvie@users.noreply.github.com>
2023-01-13 11:28:05 +0000

    Updated dependencies and refactoring due to updated dependencies (#49)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,14 +3,14 @@ import sbt._
 object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"   %% "bootstrap-frontend-play-28"    % "7.11.0",
-    "uk.gov.hmrc"   %% "play-frontend-hmrc"            % "3.34.0-play-28",
+    "uk.gov.hmrc"   %% "bootstrap-frontend-play-28"    % "7.12.0",
+    "uk.gov.hmrc"   %% "play-frontend-hmrc"            % "6.2.0-play-28",
     "uk.gov.hmrc"   %% "play-conditional-form-mapping" % "1.12.0-play-28"
   )
 
   val test: Seq[ModuleID]    = Seq(
-    "uk.gov.hmrc"          %% "bootstrap-test-play-28"   % "7.11.0",
-    "org.jsoup"             % "jsoup"                    % "1.14.3",
+    "uk.gov.hmrc"          %% "bootstrap-test-play-28"   % "7.12.0",
+    "org.jsoup"             % "jsoup"                    % "1.15.3",
     "org.mockito"          %% "mockito-scala"            % "1.17.12",
     "org.scalatestplus"    %% "scalatestplus-scalacheck" % "3.1.0.0-RC2",
     "com.danielasfregola"  %% "random-data-generator"    % "2.9",
