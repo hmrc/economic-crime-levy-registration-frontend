1b7825af1bf112edd1c1bc9a4eebd4db782dce4f
Peter Hazell <18005888+peter-hazell@users.noreply.github.com>
2023-02-09 08:23:39 +0000

    Update nonBooleans gen to only generate non-blank strings (#65)

--- test-common/uk/gov/hmrc/economiccrimelevyregistration/generators/Generators.scala
+++ test-common/uk/gov/hmrc/economiccrimelevyregistration/generators/Generators.scala
@@ -80,16 +80,12 @@ trait Generators {
   def longsOutsideRange(min: Long, max: Long): Gen[Long] =
     arbitrary[Long] suchThat (x => x < min || x > max)
 
+  def nonBlankString: Gen[String] = arbitrary[String] suchThat (!_.isBlank)
+
   def nonBooleans: Gen[String] =
-    arbitrary[String]
-      .suchThat(_.nonEmpty)
+    nonBlankString
       .suchThat(_ != "true")
       .suchThat(_ != "false")
-
-  def nonEmptyString: Gen[String] =
-    arbitrary[String] suchThat (_.nonEmpty)
-
-  def nonBlankString: Gen[String] = arbitrary[String] suchThat (!_.isBlank)
 
   def stringsWithMaxLength(maxLength: Int): Gen[String] =
     for {
