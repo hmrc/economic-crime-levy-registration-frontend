aa8b2020d43ebfb69d7a7e104203e399d7ffe42e
peter-hazell <18005888+peter-hazell@users.noreply.github.com>
2023-02-16 09:17:30 +0000

    ECL-251: Pretty print json when logging

--- app/uk/gov/hmrc/economiccrimelevyregistration/connectors/EclRegistrationConnector.scala
+++ app/uk/gov/hmrc/economiccrimelevyregistration/connectors/EclRegistrationConnector.scala
@@ -18,6 +18,7 @@ package uk.gov.hmrc.economiccrimelevyregistration.connectors
 
 import play.api.Logging
 import play.api.http.Status._
+import play.api.libs.json.Json
 import uk.gov.hmrc.economiccrimelevyregistration.config.AppConfig
 import uk.gov.hmrc.economiccrimelevyregistration.models.errors.DataValidationErrors
 import uk.gov.hmrc.economiccrimelevyregistration.models.{CreateEclSubscriptionResponse, EclSubscriptionStatus, Registration}
@@ -72,7 +73,7 @@ class EclRegistrationConnector @Inject() (appConfig: AppConfig, httpClient: Http
           httpResponse.status match {
             case NO_CONTENT => None
             case OK         =>
-              logger.warn(s"Data validation errors:\n${httpResponse.json}")
+              logger.warn(s"Data validation errors:\n${Json.prettyPrint(httpResponse.json)}")
               Some(httpResponse.json.as[DataValidationErrors])
             case s          => throw new HttpException(s"Unexpected response with HTTP status $s", s)
           }
