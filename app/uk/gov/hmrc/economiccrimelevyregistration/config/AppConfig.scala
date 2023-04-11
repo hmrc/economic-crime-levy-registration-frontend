8717b53d8edf137e63138a88ee7f6beeda0d1082
peter-hazell <18005888+peter-hazell@users.noreply.github.com>
2023-04-11 16:32:01 +0100

    Update generic GRS failure contact us link

--- app/uk/gov/hmrc/economiccrimelevyregistration/config/AppConfig.scala
+++ app/uk/gov/hmrc/economiccrimelevyregistration/config/AppConfig.scala
@@ -37,6 +37,9 @@ class AppConfig @Inject() (configuration: Configuration, servicesConfig: Service
   def feedbackUrl(backUrl: String): String =
     s"$contactHost/contact/beta-feedback?service=$contactFormServiceIdentifier&backUrl=${SafeRedirectUrl(backUrl).encodedUrl}"
 
+  def reportTechnicalIssueUrl(referrerUrl: String): String =
+    s"$contactHost/contact/report-technical-problem?service=$contactFormServiceIdentifier&referrerUrl=$referrerUrl"
+
   val signInUrl: String                 = configuration.get[String]("urls.signIn")
   val signOutUrl: String                = configuration.get[String]("urls.signOut")
   val eclSignOutUrl: String             = configuration.get[String]("urls.eclSignOut")
