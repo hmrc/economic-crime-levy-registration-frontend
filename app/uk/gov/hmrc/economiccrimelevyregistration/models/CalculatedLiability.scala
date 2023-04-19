2d942f51a6ea74b9cde9db7334bd566c5aae7c47
TonyCOgilvie <111900771+TonyCOgilvie@users.noreply.github.com>
2023-04-19 10:51:03 +0100

    Addressed PR review comment

--- app/uk/gov/hmrc/economiccrimelevyregistration/models/CalculatedLiability.scala
+++ app/uk/gov/hmrc/economiccrimelevyregistration/models/CalculatedLiability.scala
@@ -43,7 +43,7 @@ object Band {
   }
 }
 
-final case class EclAmount(amount: BigDecimal, apportioned: Boolean = false)
+final case class EclAmount(amount: BigDecimal)
 
 object EclAmount {
   implicit val format: OFormat[EclAmount] = Json.format[EclAmount]
@@ -59,8 +59,7 @@ final case class Bands(
   small: BandRange,
   medium: BandRange,
   large: BandRange,
-  veryLarge: BandRange,
-  apportioned: Boolean = false
+  veryLarge: BandRange
 )
 
 object Bands {
