d7b8d5a2442ad458a9ae82252c19cb5fd24c4e06
TonyCOgilvie <111900771+TonyCOgilvie@users.noreply.github.com>
2023-04-21 11:07:40 +0100

    Updated dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,15 +2,17 @@ import sbt._
 
 object AppDependencies {
 
+  private val hmrcBootstrapVersion = "7.15.0"
+
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"   %% "bootstrap-frontend-play-28"    % "7.12.0",
-    "uk.gov.hmrc"   %% "play-frontend-hmrc"            % "6.8.0-play-28",
-    "uk.gov.hmrc"   %% "play-conditional-form-mapping" % "1.12.0-play-28"
+    "uk.gov.hmrc"   %% "bootstrap-frontend-play-28"    % hmrcBootstrapVersion,
+    "uk.gov.hmrc"   %% "play-frontend-hmrc"            % "7.3.0-play-28",
+    "uk.gov.hmrc"   %% "play-conditional-form-mapping" % "1.13.0-play-28"
   )
 
   val test: Seq[ModuleID]    = Seq(
-    "uk.gov.hmrc"          %% "bootstrap-test-play-28"   % "7.12.0",
-    "org.jsoup"             % "jsoup"                    % "1.15.3",
+    "uk.gov.hmrc"          %% "bootstrap-test-play-28"   % hmrcBootstrapVersion,
+    "org.jsoup"             % "jsoup"                    % "1.15.4",
     "org.mockito"          %% "mockito-scala"            % "1.17.12",
     "org.scalatestplus"    %% "scalatestplus-scalacheck" % "3.1.0.0-RC2",
     "com.danielasfregola"  %% "random-data-generator"    % "2.9",
