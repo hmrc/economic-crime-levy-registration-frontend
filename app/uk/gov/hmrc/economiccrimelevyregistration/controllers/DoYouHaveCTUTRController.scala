ad525602fe51a19bbc44822f43cebe30e5a7937d
nemanjatomanovic <nemanja.tomanovic@gmail.com>
2023-06-09 12:08:30 +0100

    ECL-405 - Added initial files, starting point, initial view, form provider and custom navigator. Aligned content with confluence.

---
+++ app/uk/gov/hmrc/economiccrimelevyregistration/controllers/DoYouHaveCTUTRController.scala
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.economiccrimelevyregistration.controllers
+
+import com.google.inject.Inject
+import play.api.data.Form
+import play.api.i18n.I18nSupport
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.economiccrimelevyregistration.connectors.EclRegistrationConnector
+import uk.gov.hmrc.economiccrimelevyregistration.controllers.actions.{AuthorisedActionWithEnrolmentCheck, DataRetrievalAction}
+import uk.gov.hmrc.economiccrimelevyregistration.forms.DoYouHaveCTUTRFormProvider
+import uk.gov.hmrc.economiccrimelevyregistration.models.Mode
+import uk.gov.hmrc.economiccrimelevyregistration.navigation.DoYouHaveCTUTRPageNavigator
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import uk.gov.hmrc.economiccrimelevyregistration.views.html.DoYouHaveCTUTRView
+
+import javax.inject.Singleton
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class DoYouHaveCTUTRController @Inject() (
+  val controllerComponents: MessagesControllerComponents,
+  authorise: AuthorisedActionWithEnrolmentCheck,
+  getRegistrationData: DataRetrievalAction,
+  formProvider: DoYouHaveCTUTRFormProvider,
+  eclRegistrationConnector: EclRegistrationConnector,
+  pageNavigator: DoYouHaveCTUTRPageNavigator,
+  view: DoYouHaveCTUTRView
+)(implicit
+  ec: ExecutionContext
+) extends FrontendBaseController
+    with I18nSupport {
+
+  val form: Form[Boolean] = formProvider()
+
+  def onPageLoad(mode: Mode): Action[AnyContent] = (authorise andThen getRegistrationData) { implicit request =>
+    Ok(view(form, mode))
+  }
+
+  def onSubmit(mode: Mode): Action[AnyContent] = (authorise andThen getRegistrationData).async { implicit request =>
+    form
+      .bindFromRequest()
+      .fold(
+        formWithErrors => Future.successful(BadRequest(view(formWithErrors, mode))),
+        ctutr => {
+          val otherEntityJourneyData = request.registration.otherEntityJourneyData.copy(ctUtr = Some(ctutr.toString))
+          eclRegistrationConnector
+            .upsertRegistration(
+              request.registration.copy(optOtherEntityJourneyData = Some(otherEntityJourneyData))
+            )
+            .flatMap(updatedRegistration => pageNavigator.nextPage(mode, updatedRegistration).map(Redirect))
+        }
+      )
+  }
+}
---
+++ app/uk/gov/hmrc/economiccrimelevyregistration/navigation/DoYouHaveCTUTRPageNavigator.scala
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.economiccrimelevyregistration.navigation
+
+import play.api.mvc.{Call, RequestHeader}
+import uk.gov.hmrc.economiccrimelevyregistration.models.{CheckMode, Mode, NormalMode, Registration}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendHeaderCarrierProvider
+
+import scala.concurrent.Future
+
+class DoYouHaveCTUTRPageNavigator extends AsyncPageNavigator with FrontendHeaderCarrierProvider {
+  override protected def navigateInNormalMode(registration: Registration)(implicit
+    request: RequestHeader
+  ): Future[Call] = navigateInEitherMode(registration, NormalMode)
+
+  override protected def navigateInCheckMode(registration: Registration)(implicit
+    request: RequestHeader
+  ): Future[Call] = navigateInEitherMode(registration, CheckMode)
+
+  private def navigateInEitherMode(registration: Registration, mode: Mode): Future[Call] =
+    registration.otherEntityJourneyData.ctUtr.get match {
+      case true  => ??? // GOTO: View to add CT UTR
+      case false => ??? // GOTO: Check your answers
+    }
+}
