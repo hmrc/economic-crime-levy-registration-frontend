13f8b5f6489a51736f41272b2e54701e118d0b6a
liandra-giacomine <91252116+liandra-giacomine@users.noreply.github.com>
2023-08-18 09:37:31 +0100

    ECL-480: Updated accessibility statement link (#155)
    * Updated accessibility statement link to point to the ecl statement page
    
    * Updated accessibility statement link in integration tests

--- test/uk/gov/hmrc/economiccrimelevyregistration/connectors/PartnershipIdentificationFrontendConnectorSpec.scala
+++ test/uk/gov/hmrc/economiccrimelevyregistration/connectors/PartnershipIdentificationFrontendConnectorSpec.scala
@@ -60,7 +60,7 @@ class PartnershipIdentificationFrontendConnectorSpec extends SpecBase {
             optServiceName = Some(serviceNameLabels.en.optServiceName),
             deskProServiceId = "economic-crime-levy-registration-frontend",
             signOutUrl = "http://localhost:14000/register-for-economic-crime-levy/account/sign-out-survey",
-            accessibilityUrl = "/accessibility-statement/register-for-economic-crime-levy",
+            accessibilityUrl = "/accessibility-statement/economic-crime-levy",
             labels = serviceNameLabels
           )
         }
