57b95a489a78805284a91ac18f733a78f35b847c
Patrick Lucas <patrick.lucas@opencastsoftware.com>
2023-12-11 15:26:48 +0000

    ECL-683: Revised title for uk revenue

--- it/uk/gov/hmrc/economiccrimelevyregistration/UkRevenueISpec.scala
+++ it/uk/gov/hmrc/economiccrimelevyregistration/UkRevenueISpec.scala
@@ -31,7 +31,7 @@ class UkRevenueISpec extends ISpecBase with AuthorisedBehaviour {
 
       status(result) shouldBe OK
 
-      html(result) should include("What was your total UK revenue in your relevant accounting period?")
+      html(result) should include("Total UK revenue")
     }
   }
 
