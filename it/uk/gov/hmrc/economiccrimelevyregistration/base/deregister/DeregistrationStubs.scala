0cd41eb8b16492adb7d1e3ebbacdede377cb43c0
Patrick Lucas <patrick.lucas@opencastsoftware.com>
2024-02-14 10:28:09 +0000

    ECL-707: Deregister journey

---
+++ it/uk/gov/hmrc/economiccrimelevyregistration/base/deregister/DeregistrationStubs.scala
@@ -0,0 +1,39 @@
+package uk.gov.hmrc.economiccrimelevyregistration.base.deregister
+
+import com.github.tomakehurst.wiremock.client.WireMock._
+import com.github.tomakehurst.wiremock.stubbing.StubMapping
+import play.api.http.Status.OK
+import play.api.libs.json.Json
+import uk.gov.hmrc.economiccrimelevyregistration.base.WireMockHelper._
+import uk.gov.hmrc.economiccrimelevyregistration.base.WireMockStubs
+import uk.gov.hmrc.economiccrimelevyregistration.models.RegistrationAdditionalInfo
+import uk.gov.hmrc.economiccrimelevyregistration.models.deregister.Deregistration
+
+trait DeregistrationStubs { self: WireMockStubs =>
+
+  def stubGetDeregistration(deregistration: Deregistration): StubMapping =
+    stub(
+      get(urlEqualTo(s"/economic-crime-levy-registration/deregistration/$testInternalId")),
+      aResponse()
+        .withStatus(OK)
+        .withBody(Json.toJson(deregistration.copy(internalId = testInternalId)).toString())
+    )
+
+  def stubUpsertDeregistration(deregistration: Deregistration): StubMapping =
+    stub(
+      put(urlEqualTo("/economic-crime-levy-registration/deregistration"))
+        .withRequestBody(
+          equalToJson(Json.toJson(deregistration).toString(), true, true)
+        ),
+      aResponse()
+        .withStatus(OK)
+    )
+
+  def stubDeleteDeregistration(): StubMapping =
+    stub(
+      delete(urlEqualTo(s"/economic-crime-levy-registration/deregistration/$testInternalId")),
+      aResponse()
+        .withStatus(OK)
+    )
+
+}
---
+++ it/uk/gov/hmrc/economiccrimelevyregistration/deregister/DeregusterReasonISpec.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.economiccrimelevyregistration.deregister
+
+import com.danielasfregola.randomdatagenerator.RandomDataGenerator.random
+import play.api.test.FakeRequest
+import uk.gov.hmrc.economiccrimelevyregistration.base.ISpecBase
+import uk.gov.hmrc.economiccrimelevyregistration.behaviours.AuthorisedBehaviour
+import uk.gov.hmrc.economiccrimelevyregistration.generators.CachedArbitraries._
+import uk.gov.hmrc.economiccrimelevyregistration.models.NormalMode
+import uk.gov.hmrc.economiccrimelevyregistration.models.deregister.Deregistration
+
+class DeregusterReasonISpec extends ISpecBase with AuthorisedBehaviour {
+
+  s"GET ${uk.gov.hmrc.economiccrimelevyregistration.controllers.deregister.routes.DeregisterReasonController.onPageLoad(NormalMode).url}" should {
+    behave like authorisedActionWithoutEnrolmentCheckRoute(
+      uk.gov.hmrc.economiccrimelevyregistration.controllers.deregister.routes.DeregisterReasonController
+        .onPageLoad(NormalMode)
+    )
+
+    "respond with 200 status and the deregister reason HTML view" in {
+      stubAuthorisedWithEclEnrolment()
+      stubGetDeregistration(random[Deregistration])
+
+      val result = callRoute(
+        FakeRequest(
+          uk.gov.hmrc.economiccrimelevyregistration.controllers.deregister.routes.DeregisterReasonController
+            .onPageLoad(NormalMode)
+        )
+      )
+
+      status(result) shouldBe OK
+      html(result)     should include("Select the reason for deregistering")
+    }
+  }
+
+}
