cbaf67c8f811250df58d065fa441376149401bf1
Daniel P. Doyle <3020488+TheDanielDoyle@users.noreply.github.com>
2024-02-26 20:08:55 +0000

    ECL-436: Corrected failing test

--- it/uk/gov/hmrc/economiccrimelevyregistration/BusinessNameISpec.scala
+++ it/uk/gov/hmrc/economiccrimelevyregistration/BusinessNameISpec.scala
@@ -6,7 +6,7 @@ import uk.gov.hmrc.economiccrimelevyregistration.base.ISpecBase
 import uk.gov.hmrc.economiccrimelevyregistration.behaviours.AuthorisedBehaviour
 import uk.gov.hmrc.economiccrimelevyregistration.controllers.routes
 import uk.gov.hmrc.economiccrimelevyregistration.generators.CachedArbitraries._
-import uk.gov.hmrc.economiccrimelevyregistration.models.EntityType.{Charity, NonUKEstablishment, Trust, UnincorporatedAssociation}
+import uk.gov.hmrc.economiccrimelevyregistration.models.EntityType.{Charity, NonUKEstablishment, Trust, UnincorporatedAssociation, UnlimitedCompany}
 import uk.gov.hmrc.economiccrimelevyregistration.models._
 
 class BusinessNameISpec extends ISpecBase with AuthorisedBehaviour {
@@ -62,7 +62,10 @@ class BusinessNameISpec extends ISpecBase with AuthorisedBehaviour {
         )
 
       val registration = random[Registration]
-        .copy(optOtherEntityJourneyData = Some(otherEntityJourneyData))
+        .copy(
+          optOtherEntityJourneyData = Some(otherEntityJourneyData),
+          entityType = Some(UnlimitedCompany)
+        )
 
       val additionalInfo = random[RegistrationAdditionalInfo]
 
