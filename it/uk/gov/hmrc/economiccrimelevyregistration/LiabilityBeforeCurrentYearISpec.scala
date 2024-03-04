48597c27ee0e829369f0512180b785f6950efd21
Patrick Lucas <patrick.lucas@opencastsoftware.com>
2024-03-04 17:42:48 +0000

    ECL-798: Wrong screen fix

--- it/uk/gov/hmrc/economiccrimelevyregistration/LiabilityBeforeCurrentYearISpec.scala
+++ it/uk/gov/hmrc/economiccrimelevyregistration/LiabilityBeforeCurrentYearISpec.scala
@@ -9,6 +9,8 @@ import uk.gov.hmrc.economiccrimelevyregistration.generators.CachedArbitraries._
 import uk.gov.hmrc.economiccrimelevyregistration.models.RegistrationType.Initial
 import uk.gov.hmrc.economiccrimelevyregistration.models._
 
+import java.time.LocalDate
+
 class LiabilityBeforeCurrentYearISpec extends ISpecBase with AuthorisedBehaviour {
 
   s"GET ${routes.LiabilityBeforeCurrentYearController.onPageLoad(NormalMode).url}" should {
@@ -38,15 +40,15 @@ class LiabilityBeforeCurrentYearISpec extends ISpecBase with AuthorisedBehaviour
     }
   }
 
-  s"POST ${routes.LiabilityBeforeCurrentYearController.onSubmit(CheckMode).url}"   should {
+  s"POST ${routes.LiabilityBeforeCurrentYearController.onSubmit(NormalMode).url}"  should {
     behave like authorisedActionWithEnrolmentCheckRoute(
-      routes.LiabilityBeforeCurrentYearController.onSubmit(CheckMode)
+      routes.LiabilityBeforeCurrentYearController.onSubmit(NormalMode)
     )
 
-    "save the selected address option then redirect to the address lookup frontend journey" in {
+    "save the selected address option then redirect to the liability date page" in {
       stubAuthorisedWithNoGroupEnrolment()
 
-      val liableBeforeCurrentYear = random[Boolean]
+      val liableBeforeCurrentYear = true
       val registration            = random[Registration]
         .copy(
           entityType = Some(random[EntityType]),
@@ -72,6 +74,94 @@ class LiabilityBeforeCurrentYearISpec extends ISpecBase with AuthorisedBehaviour
       stubUpsertRegistration(registration)
 
       val result = callRoute(
+        FakeRequest(routes.LiabilityBeforeCurrentYearController.onSubmit(NormalMode))
+          .withFormUrlEncodedBody(("value", liableBeforeCurrentYear.toString))
+      )
+
+      status(result) shouldBe SEE_OTHER
+
+      redirectLocation(result) shouldBe Some(routes.LiabilityDateController.onPageLoad(NormalMode).url)
+    }
+
+    "save the selected address option then redirect entity type page" in {
+      stubAuthorisedWithNoGroupEnrolment()
+
+      val liableBeforeCurrentYear = false
+      val registration            = random[Registration]
+        .copy(
+          entityType = Some(random[EntityType]),
+          registrationType = Some(Initial),
+          relevantApRevenue = Some(randomApRevenue()),
+          businessSector = None,
+          revenueMeetsThreshold = Some(true)
+        )
+      val additionalInfo          = random[RegistrationAdditionalInfo]
+
+      stubGetRegistrationAdditionalInfo(additionalInfo)
+      stubGetRegistration(registration)
+
+      val info = RegistrationAdditionalInfo(
+        registration.internalId,
+        None,
+        None,
+        None,
+        None,
+        None
+      )
+
+      stubUpsertRegistrationAdditionalInfo(info)
+      stubUpsertRegistration(registration)
+
+      val result = callRoute(
+        FakeRequest(routes.LiabilityBeforeCurrentYearController.onSubmit(NormalMode))
+          .withFormUrlEncodedBody(("value", liableBeforeCurrentYear.toString))
+      )
+
+      status(result) shouldBe SEE_OTHER
+
+      redirectLocation(result) shouldBe Some(routes.EntityTypeController.onPageLoad(NormalMode).url)
+    }
+  }
+
+  s"POST ${routes.LiabilityBeforeCurrentYearController.onSubmit(CheckMode).url}"   should {
+    behave like authorisedActionWithEnrolmentCheckRoute(
+      routes.LiabilityBeforeCurrentYearController.onSubmit(CheckMode)
+    )
+
+    "redirect to check your answers page if nothing changed" in {
+      stubAuthorisedWithNoGroupEnrolment()
+
+      val liableBeforeCurrentYear = random[Boolean]
+      val registration            = random[Registration]
+        .copy(
+          entityType = Some(random[EntityType]),
+          registrationType = Some(Initial),
+          relevantApRevenue = Some(randomApRevenue()),
+          businessSector = None,
+          revenueMeetsThreshold = Some(true)
+        )
+      val additionalInfo          =
+        random[RegistrationAdditionalInfo].copy(
+          liableForPreviousYears = Some(liableBeforeCurrentYear),
+          liabilityStartDate = Some(random[LocalDate])
+        )
+
+      stubGetRegistrationAdditionalInfo(additionalInfo)
+      stubGetRegistration(registration)
+
+      val info = RegistrationAdditionalInfo(
+        registration.internalId,
+        None,
+        None,
+        None,
+        None,
+        None
+      )
+
+      stubUpsertRegistrationAdditionalInfo(info)
+      stubUpsertRegistration(registration)
+
+      val result = callRoute(
         FakeRequest(routes.LiabilityBeforeCurrentYearController.onSubmit(CheckMode))
           .withFormUrlEncodedBody(("value", liableBeforeCurrentYear.toString))
       )
@@ -79,6 +169,48 @@ class LiabilityBeforeCurrentYearISpec extends ISpecBase with AuthorisedBehaviour
       status(result) shouldBe SEE_OTHER
 
       redirectLocation(result) shouldBe Some(routes.CheckYourAnswersController.onPageLoad().url)
+    }
+
+    "redirect to liability date page if change to yes" in {
+      stubAuthorisedWithNoGroupEnrolment()
+
+      val liableBeforeCurrentYear = true
+      val registration            = random[Registration]
+        .copy(
+          entityType = Some(random[EntityType]),
+          registrationType = Some(Initial),
+          relevantApRevenue = Some(randomApRevenue()),
+          businessSector = None
+        )
+      val additionalInfo          =
+        random[RegistrationAdditionalInfo].copy(
+          liableForPreviousYears = Some(!liableBeforeCurrentYear),
+          liabilityStartDate = None
+        )
+
+      stubGetRegistrationAdditionalInfo(additionalInfo)
+      stubGetRegistration(registration)
+
+      val info = RegistrationAdditionalInfo(
+        registration.internalId,
+        None,
+        None,
+        None,
+        None,
+        None
+      )
+
+      stubUpsertRegistrationAdditionalInfo(info)
+      stubUpsertRegistration(registration)
+
+      val result = callRoute(
+        FakeRequest(routes.LiabilityBeforeCurrentYearController.onSubmit(CheckMode))
+          .withFormUrlEncodedBody(("value", liableBeforeCurrentYear.toString))
+      )
+
+      status(result) shouldBe SEE_OTHER
+
+      redirectLocation(result) shouldBe Some(routes.LiabilityDateController.onPageLoad(CheckMode).url)
     }
   }
 }
