21026b40de761f34d23283a343188ab01f1f2b5f
Nemanja Tomanovic <nemanja.tomanovic@gmail.com>
2024-04-02 14:48:28 +0100

    ECL-865: Accessibility Issues - Page level headings (H1) on same page

--- app/uk/gov/hmrc/economiccrimelevyregistration/viewmodels/govuk/RadiosFluency.scala
+++ app/uk/gov/hmrc/economiccrimelevyregistration/viewmodels/govuk/RadiosFluency.scala
@@ -19,7 +19,6 @@ package uk.gov.hmrc.economiccrimelevyregistration.viewmodels.govuk
 import play.api.data.Field
 import play.api.i18n.Messages
 import uk.gov.hmrc.economiccrimelevyregistration.viewmodels.ErrorMessageAwareness
-import uk.gov.hmrc.govukfrontend.views.viewmodels.content.Text
 import uk.gov.hmrc.govukfrontend.views.viewmodels.fieldset.{Fieldset, Legend}
 import uk.gov.hmrc.govukfrontend.views.viewmodels.hint.Hint
 import uk.gov.hmrc.govukfrontend.views.viewmodels.radios.{RadioItem, Radios}
@@ -52,40 +51,6 @@ trait RadiosFluency {
         items = items map (item => item.copy(checked = field.value.isDefined && field.value == item.value)),
         errorMessage = errorMessage(field, Some(messages("govukErrorMessage.visuallyHiddenText")))
       )
-
-    def yesNo(
-      field: Field,
-      legend: Legend
-    )(implicit messages: Messages): Radios =
-      yesNo(
-        field = field,
-        fieldset = FieldsetViewModel(legend)
-      )
-
-    def yesNo(
-      field: Field,
-      fieldset: Fieldset
-    )(implicit messages: Messages): Radios = {
-
-      val items = Seq(
-        RadioItem(
-          id = Some(field.id),
-          value = Some("true"),
-          content = Text(messages("site.yes"))
-        ),
-        RadioItem(
-          id = Some(s"${field.id}-no"),
-          value = Some("false"),
-          content = Text(messages("site.no"))
-        )
-      )
-
-      apply(
-        field = field,
-        fieldset = fieldset,
-        items = items
-      ).inline()
-    }
   }
 
   implicit class FluentRadios(radios: Radios) {
@@ -93,19 +58,10 @@ trait RadiosFluency {
     def withHint(hint: Hint): Radios =
       radios.copy(hint = Some(hint))
 
-    def withFormGroupClasses(classes: String): Radios =
-      radios.copy(formGroupClasses = classes)
-
-    def withIdPrefix(prefix: String): Radios =
-      radios.copy(idPrefix = Some(prefix))
-
     def withCssClass(newClass: String): Radios =
       radios.copy(classes = s"${radios.classes} $newClass")
 
     def withAttribute(attribute: (String, String)): Radios =
       radios.copy(attributes = radios.attributes + attribute)
-
-    def inline(): Radios =
-      radios.withCssClass("govuk-radios--inline")
   }
 }
