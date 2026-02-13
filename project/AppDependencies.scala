f8bc7fb8fe5c25d9eadc32c9ccdb2f82f3e36dd4
Athish Thayalan <120419238+AthishThayalan@users.noreply.github.com>
2026-02-13 13:20:41 +0000

    Upgrade dependencies (#383)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import sbt._
 
 object AppDependencies {
 
-  private val hmrcBootstrapVersion = "9.18.0"
+  private val hmrcBootstrapVersion = "10.5.0"
   private val playVersion          = "30"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"   %% s"bootstrap-frontend-play-$playVersion"            % hmrcBootstrapVersion,
-    "uk.gov.hmrc"   %% s"play-frontend-hmrc-play-$playVersion"            % "12.8.0",
-    "uk.gov.hmrc"   %% s"play-conditional-form-mapping-play-$playVersion" % "3.3.0",
+    "uk.gov.hmrc"   %% s"play-frontend-hmrc-play-$playVersion"            % "12.31.0",
+    "uk.gov.hmrc"   %% s"play-conditional-form-mapping-play-$playVersion" % "3.4.0",
     "org.typelevel" %% "cats-core"                                        % "2.13.0"
   )
 
