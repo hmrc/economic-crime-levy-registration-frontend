ccaeb1bf5850ce030d867731421b15faad48e2b2
Antony100 <antony.angeli@mercator.group>
2026-07-07 17:35:36 +0100

    add research banner to RegistrationReceived page

--- app/uk/gov/hmrc/economiccrimelevyregistration/config/AppConfig.scala
+++ app/uk/gov/hmrc/economiccrimelevyregistration/config/AppConfig.scala
@@ -93,4 +93,8 @@ class AppConfig @Inject() (configuration: Configuration, servicesConfig: Service
 
   val amlProfessionalBodySupervisors: Seq[String] = configuration.get[Seq[String]]("amlProfessionalBodySupervisors")
 
+  val researchBannerEnabled: Boolean = configuration.get[Boolean]("features.researchBannerEnabled")
+
+  val researchBannerUrl: String = configuration.get[String]("urls.researchBanner")
+
 }
